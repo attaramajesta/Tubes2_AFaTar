6b51e11ffc34081677fcc1719a0fdc027d2d62b9
attaramajesta <78683973+attaramajesta@users.noreply.github.com>
2024-04-27 20:44:33 +0700

    update frontend

--- src/frontend/src/features/race/Race.jsx
+++ src/frontend/src/features/race/Race.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import Heart from '../../assets/heart.png';
+import Energy from '../../assets/energy.png';
+import MarioBFS from '../../assets/bfs-mario.png';
+import LuigiIDS from '../../assets/ids-luigi.png';
 import "./Race.css"
 
 const Race = () => {
@@ -7,6 +11,10 @@ const Race = () => {
     const [startInput, setStartInput] = useState("");
     const [destInput, setDestInput] = useState("");
     const [algoSwitch, setAlgoSwitch] = useState(false);
+    const [showResultPage, setShowResultPage] = useState(false);
+    const [resultData, setResultData] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [buttonClicked, setButtonClicked] = useState(false);
 
     const handleInputAwal = async (e) => {
         const query = e.target.value;
@@ -37,7 +45,7 @@ const Race = () => {
     }
 
     const fetchPath = async () => {
-
+        setLoading(true);
         try {
             const algorithm = algoSwitch ? "ids" : "bfs";
             const startInputFormatted = startInput.replace(/\s/g, "_");
@@ -48,45 +56,80 @@ const Race = () => {
             }
             const data = await response.json();
             console.log(data);
+            setResultData(data);
+            setLoading(false);
         } catch (error) {
+            setLoading(false);
             console.error('Fetch error:', error);
         }
     };
 
-    return(
+    const ResultPage = () => {
+        return (
+            <div className="result-box">
+                <h2 className="title">Result</h2>
+                <p>Duration: {resultData.duration / 1000} s </p>
+                <p>Path:</p>
+                <ul>
+                    {resultData.path.map((step, index) => (
+                        <li key={index}><a href={`https://en.wikipedia.org/wiki/${step.URL}`}>{step.URL}</a></li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+      
+    const changeColor = () => {
+        var button = document.getElementById("raceButton");
+        button.classList.add("clicked");
+        setTimeout(() => {
+            button.classList.remove("clicked");
+        }, 200);
+    }
+
+    return (
         <div id="race" className="race-page">
-            <div className='title'>
-                <h2>Find the shortest path from</h2>
-            </div>
-            <div className='input-box'>
-                <input type="text" id="startInput" placeholder="Superman" value={startInput} onChange={handleInputAwal}/>
-                <div className="suggestion">
-                    {suggestionAwal.map((suggestion, index) => (
-                        <p key={index} onClick={() => {setStartInput(suggestion); setSuggestionsAwal([]);}}>{suggestion}</p>
-                    ))}
+            <div className="race-container">
+                <img src={Heart} className="stat" alt="heart rate"/>
+                <img src={Energy} className="stat" alt="heart rate"/>
+                <img src={algoSwitch ? LuigiIDS : MarioBFS} alt={algoSwitch ? 'MarioBFS' : 'LuigiIDS'} className="choose-character"/>
+                <div className="race-box">
+                    <div className="race-title">
+                        <h2>Find the shortest path from</h2>
+                    </div>
+                    <div className='input-box'>
+                        <input autocomplete="off" type="text" id="startInput" placeholder="Superman" value={startInput} onChange={handleInputAwal}/>
+                        <div className="suggestion">
+                            {suggestionAwal.map((suggestion, index) => (
+                                <p key={index} onClick={() => { setStartInput(suggestion); setSuggestionsAwal([]); }}>{suggestion}</p>
+                            ))}
+                        </div>
+                    </div>
+                    <h2 className="to">to</h2>
+                    <div className='input-box'>
+                        <input autocomplete="off" type="text" id="destInput" placeholder="Marine" value={destInput} onChange={handleInputAkhir} />
+                        <div className="suggestion">
+                            {suggestionAkhir.map((suggestion, index) => (
+                                <p key={index} onClick={() => { setDestInput(suggestion); setSuggestionsAkhir([]); }}>{suggestion}</p>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='switch-container'>
+                        <span>BFS</span>
+                        <label className="switch">
+                            <input type="checkbox" id="algoSwitch" checked={algoSwitch} onChange={e => setAlgoSwitch(e.target.checked)} />
+                            <span className="slider round"></span>
+                        </label>
+                        <span>IDS</span>
+                    </div>
+                    <div className='submit-container'>
+                        <button id="raceButton" onClick={() => {fetchPath(); changeColor();}}>RACE!</button>
+                    </div>
                 </div>
             </div>
-            <div className='input-box'>
-                <input type="text" id="destInput" placeholder="Marine" value={destInput} onChange={handleInputAkhir}/>
-                <div className="suggestion">
-                    {suggestionAkhir.map((suggestion, index) => (
-                        <p key={index} onClick={() => {setDestInput(suggestion); setSuggestionsAkhir([]);}}>{suggestion}</p>
-                    ))}
-                </div>
-            </div>
-            <div className='switch-container'>
-                <span>BFS</span>
-                <label className="switch">
-                    <input type="checkbox" id="algoSwitch" checked={algoSwitch} onChange={e => setAlgoSwitch(e.target.checked)}></input>
-                    <span className="slider round"></span>
-                </label>
-                <span>IDS</span>
-            </div>
-            <div className='submit-container'>
-                <button onClick={fetchPath}>RACE!</button>
-            </div>
-      </div>
+            <div className="highlight"></div>
+            {showResultPage && <ResultPage />}
+        </div>
     );
 }
-
 export default Race;
